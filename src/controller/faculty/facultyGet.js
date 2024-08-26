6beab531ac6e5554c8994d34a716506f88db2dcc
Shubham Thakur <shubham58188@gmail.com>
2024-08-26 11:35:15 +0530

    added route to get faculties

---
+++ src/controller/faculty/facultyGet.js
@@ -0,0 +1,29 @@
+const Faculty = require("../../models/faculty");
+
+const showFaculties = async (req, res) => {
+    try {
+        
+        const {role} = req.params;
+
+        const query = {};
+        
+        if (role) {
+            query.role = role;
+        }
+
+        const faculties = await Faculty.find(query, "-password");
+
+        if (!faculties || faculties.length === 0) {
+            return res.status(404).send("Faculties not found!");
+        }
+        return res.status(200).send(faculties);
+
+    } catch (error) {
+
+        console.log(error.message);
+        return res.status(500).send("Error occured. Please try Again");
+
+    }
+}
+
+module.exports = {showFaculties};
--- src/routes/facultyRoutes.js
+++ src/routes/facultyRoutes.js
@@ -5,11 +5,13 @@ const { auth, roleAuth } = require("../middleware/auth");
 
 const { addFaculty, facultyLogin } = require("../controller/faculty/facultyPost");
 const { removeFaculty } = require("../controller/faculty/facultyDelete");
+const {showFaculties} = require("../controller/faculty/facultyGet");
 
 //login route
 router.post("/faculty-login", facultyLogin);
 
 //other faculty routes
+router.get("/show-faculty/:role", auth, showFaculties);
 router.post("/add-faculty", auth, roleAuth("Admin"), addFaculty);
 router.delete("/remove-faculty", auth, roleAuth("Admin"), removeFaculty);
 

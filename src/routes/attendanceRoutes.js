344981d121e3d63c67cab920c955cd89aad80ca6
Shubham Thakur <shubham58188@gmail.com>
2024-08-30 22:18:31 +0530

    removed repeating code

--- src/routes/attendanceRoutes.js
+++ src/routes/attendanceRoutes.js
@@ -11,8 +11,7 @@ const { auth, roleAuth } = require("../middleware/auth");
 
 router.post("/record-new", auth, roleAuth("Faculty"), newAttendance);
 
-router.get("/show-attendance-student/:enrollNo", auth, roleAuth("Student"), getAttendanceByStudent); //this and next function are same just the role is changed in case if parent or student
-router.get("/show-attendance-parent/:enrollNo", auth, roleAuth("Parent"), getAttendanceByStudent);
+router.get("/show-attendance-student/:enrollNo", auth, getAttendanceByStudent); //this and next function are same just the role is changed in case if parent or student
 router.get("/show-attendance-date", auth, getAttendanceByDate);
 router.get("/show-attendance-faculty/:courseId/:year/:section", auth, roleAuth("Faculty"), getAttendanceByFaculty);
 

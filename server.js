0bb257bb607335931be5d20b72844f1d0525aef7
Shubham Thakur <shubham58188@gmail.com>
2024-09-09 10:34:37 +0530

    file conflict

--- server.js
+++ server.js
@@ -5,24 +5,19 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const connectDb = require("./src/database/connect"); //require connectDb function from the src -> database -> connect.js
 
-const { auth } = require("./src/middleware/auth");
-
 const courseRoutes = require("./src/routes/courseRoutes");
 const studentRoutes = require("./src/routes/studentRoutes");
 const attendanceRoutes = require("./src/routes/attendanceRoutes");
 const facultyRoutes = require("./src/routes/facultyRoutes");
 const parentRoutes = require("./src/routes/parentRoutes");
 
-
 const port = process.env.PORT || 5000;
 const app = express();
 
-
 // middlewares
 app.use(bodyParser.json());
-app.use(express.json()); 
+app.use(express.json());
 app.use(cors());
-
 
 app.use("/api/course", courseRoutes); //all the course routes are going to be here
 app.use("/api/student", studentRoutes); //all the student routes are going to be here
@@ -30,15 +25,9 @@ app.use("/api/attendance", attendanceRoutes); //all the student routes are going
 app.use("/api/faculty", facultyRoutes); //all the faculty routes are going to be here
 app.use("/api/parents", parentRoutes); //all the parent routes are going to be here
 
-
 app.get("/", (req, res) => {
     res.send("Welcome to my API");
 });
-
-// app.post("/api/sms-delivery-status",(req,res)=>){
-
-// }
-
 
 const start = async () => {
     try {
